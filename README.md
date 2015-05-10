6363d5109f3b759b5625d8d79d69bbb41b3ef559
Joseph <jsu800@gmail.com>
2015-05-10 11:32:54 -0700

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+===========================
+Android Fused Location Demo
+===========================
+
+This demo runs on Android's Fused Location Provider API:
+
+https://developer.android.com/reference/com/google/android/gms/location/FusedLocationProviderApi.html
+
+With combined use of network cellular signals, WiFi signals, and GPS signals to approximate the user's 
+location. Testing shows much improved approximation of locations with use of this API over the conventional
+means.
--- src/com/williamquast/androidlocationdemo/ErrorDialogFragment.java
+++ src/com/williamquast/androidlocationdemo/ErrorDialogFragment.java
@@ -1,8 +1,29 @@
-package com.williamquast.androidlocationdemo;
+/*
+ * Copyright (c) 2014 Joseph Su
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.josephsu.androidlocationdemo;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+
+/*
+ * @author Joseph Su
+ * 
+ */
 
 public class ErrorDialogFragment extends DialogFragment {
 	// Global field to contain the error dialog
